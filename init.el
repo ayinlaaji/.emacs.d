c11da1248193189b7edbe3d7141b6059921c4a0a
Abdulsalam Ayinla <ayinlaabdulsalam@gmail.com>
2020-06-16 20:27:43 +0100

    enable ibuffer

--- init.el
+++ init.el
@@ -65,7 +65,7 @@
        :emacs
        dired             ; making dired pretty [functional]
        electric          ; smarter, keyword-based electric-indent
-       ;;ibuffer         ; interactive buffer management
+       ibuffer         ; interactive buffer management
        vc                ; version-control and Emacs, sitting in a tree
 
        :term
